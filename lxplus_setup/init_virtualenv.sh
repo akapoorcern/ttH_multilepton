33c30419dd8156617eb5a039a52635a89ff78466
Joshuha Thomas-Wilsker <joshuhathomas-wilsker@jtwmac.dyndns.cern.ch>
2018-05-08 15:23:05 +0200

    Adding lxplus_setup directory

---
+++ lxplus_setup/init_virtualenv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VIRTUALENV_PATH=py2_virtualenv
+
+virtualenv $VIRTUALENV_PATH
+source $VIRTUALENV_PATH/bin/activate
+pip install --upgrade pip
+pip install tensorflow==1.7.0
+pip install keras==2.1.6
+pip install h5py==2.7.1
+pip install matplotlib==2.1.0
+pip install pypng==0.0.18
+pip install scikit-learn==0.19.1
+pip install keras --upgrade
+pip install tensorflow --upgrade
