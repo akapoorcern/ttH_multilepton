081c196bb0fe771cecfbff16257ae66f41e231ad
Joshuha Thomas-Wilsker <joshuhathomas-wilsker@wilskers-mac.dyndns.cern.ch>
2018-09-13 18:05:13 +0200

    add lxbatch submission scripts

---
+++ LHFit/templates/lxbatch_runjob_template_creator_ttH_category_em.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+WORKING_DIR=/afs/cern.ch/work/j/jthomasw/private/IHEP/ttHML/github/ttH_multilepton/LHFit/templates
+cd ${WORKING_DIR}
+python shape_template_creator.py -j /afs/cern.ch/work/j/jthomasw/private/IHEP/ttHML/github/ttH_multilepton/LHFit/templates/full_samples_list_17Vars.json -o /afs/cern.ch/work/j/jthomasw/private/IHEP/ttHML/github/ttH_multilepton/LHFit/templates/DNN_templates_2018_09_04 -b ttH_2LSS_em_ttHCat
---
+++ LHFit/templates/lxbatch_submit_template_creator.sh
@@ -0,0 +1,7 @@
+bsub -q 8nh -J templater_ee < lxbatch_runjob_template_creator_category_ee.sh
+bsub -q 8nh -J templater_ttHem < lxbatch_runjob_template_creator_ttH_category_em.sh
+bsub -q 8nh -J templater_ttHmm < lxbatch_runjob_template_creator_ttH_category_mm.sh
+bsub -q 8nh -J templater_ttVem < lxbatch_runjob_template_creator_ttV_category_em.sh
+bsub -q 8nh -J templater_ttVmm < lxbatch_runjob_template_creator_ttV_category_mm.sh
+bsub -q 8nh -J templater_ttJem < lxbatch_runjob_template_creator_ttJ_category_em.sh
+bsub -q 8nh -J templater_ttJmm < lxbatch_runjob_template_creator_ttJ_category_mm.sh
