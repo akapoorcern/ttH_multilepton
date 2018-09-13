f5a1ea5658b8080dee51486c1dc86425bb7b6758
Joshuha Thomas-Wilsker <joshuhathomas-wilsker@wilskers-mac.dyndns.cern.ch>
2018-09-13 17:57:39 +0200

    update lxbatch scripts

--- DNN/Evaluation/lxbatch_runjob_evaluateDNN_Raressample.sh
+++ DNN/Evaluation/lxbatch_runjob_evaluateDNN_Raressample.sh
@@ -6,4 +6,4 @@ pwd
 scl enable python27 bash
 source /cvmfs/sft.cern.ch/lcg/views/LCG_93/x86_64-slc6-gcc62-opt/setup.sh
 #source /afs/cern.ch/work/j/jthomasw/private/IHEP/ttHML/github/ttH_multilepton/lxplus_setup/py2_virtualenv/bin/activate
-python evaluate_DNN.py -j /afs/cern.ch/work/j/jthomasw/private/IHEP/ttHML/github/ttH_multilepton/DNN/input_variables_list_allJets-2018-06-15.json -s 2HLs_relu_D+G-VarTrans_0.008-learnRate_40-epochs -i /afs/cern.ch/work/j/jthomasw/private/IHEP/ttHML/github/ttH_multilepton/DNN/samples/2017_updated_MC/2LSS/Rares_2LSS.root
+python evaluate_DNN.py -j /afs/cern.ch/work/j/jthomasw/private/IHEP/ttHML/github/ttH_multilepton/DNN/input_variables_list_17Var-2018-08-22.json -s 2HLs_relu_D+G-VarTrans_0.008-learnRate_10-epochs -i /afs/cern.ch/work/j/jthomasw/private/IHEP/ttHML/github/ttH_multilepton/DNN/samples/2LSS/2018-07-26/Rares_2LSS.root
